a0c923544530c6be78e61aa25237a06f147b540d
wpghks <wpghks56@naver.com>
2023-11-23 17:31:00 +0900

    [feature] 똥 코드 보류

--- src/config/decorators/graphQL.decorator.ts
+++ src/config/decorators/graphQL.decorator.ts
@@ -5,6 +5,15 @@ export const GraphQLInfo = createParamDecorator(
     (data: unknown, context: ExecutionContext) => {
         const ctx = GqlExecutionContext.create(context);
         const info = ctx.getInfo();
+        // const requestStrings: string[] = info.fieldNodes[0].selectionSet.selections.map((selection: any) => selection.name.value);
+        // const datas = info.fieldNodes[0].selectionSet.selections.map((selection: any) => selection);
+        
+        // console.log(datas.map((selection: any) => {
+        //     if (selection.selectionSet && selection.selectionSet.selections) {
+        //         return selection.selectionSet.selections.map((selection: any) => selection.name.value);
+        //     }
+        // }));
+        
 
         if(info) {
             return info.fieldNodes[0].selectionSet.selections.map((selection: any) => selection.name.value);
